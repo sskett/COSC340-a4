da5d2afb3ae2ec21d92d445e2e44749aca5d6c8e
Spencer Skett <sskett@gmail.com>
2020-04-14 16:39:29 +0800

    Basic client-server functionality working Next step: implement hangman game

--- client.py
+++ client.py
@@ -1,21 +1,56 @@
 #!/usr/bin/env python3
 
+import sys
 import socket
 
 # Define globals
-HOST = "localhost"
-PORT = 65432
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
 encoding = 'ascii'
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    entry = (input("What do you want to say?")).encode(encoding)
-    while entry:
-        s.sendall(entry)
+def start_game(s):
+    try:
+        s.connect((HOST, PORT))
+        s.sendall(("START GAM").encode(encoding))
         data = s.recv(1024)
         data = data.decode(encoding)
-        print(str(data))
-        entry = (input("What do you want to say?")).encode(encoding)
+        if data == "Invalid start request. Disconnecting...":
+            print(data)
+            s.close()
+            return False
+        else:
+            print(data)
+            return True
+    except socket.error as err:
+        print(err)
+        s.close()
+        return False
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    active = start_game(s)
+    while active:
+        data = ""
+        entry = input("What do you want to say?")
+        if entry == "{quit}" or entry == "":
+            print("Exiting game...")
+            s.close()
+            active = False
+        else:
+            try:
+                s.sendall(entry.encode(encoding))
+                data = s.recv(1024)
+            except socket.error as err:
+                print("Connection lost:", err)
+                active = False
+                break
+            if len(data) == 0:
+                print("Sorry, connection to the server has been lost. Exiting game...")
+                active = False
+                break
+            else:
+                data = data.decode(encoding)
+                print(str(data))
 
 
--- server.py
+++ server.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python3
 
+import sys
 import socket
 
-HOST = ''
-PORT = 65432
+
+HOST = 'localhost'
+PORT = int(sys.argv[1])
 MAX_CONNS = 1
 encoding = 'ascii'
 
 
-def respond_to_user(data):
+def respond_to_user(data, conn):
     if data.decode(encoding) == 'START GAME':
-        conn.sendall(b'OK')
+        print("Starting game")
     else:
         conn.sendall(data)
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(MAX_CONNS)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
+def play_hangman():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(MAX_CONNS)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
             data = conn.recv(1024)
-            print('User says: ' + data.decode(encoding))
-            if not data: break
-            respond_to_user(data)
+            if not data.decode(encoding) == 'START GAME':
+                msg = "Invalid start request. Disconnecting..."
+                conn.sendall(msg.encode(encoding))
+                s.close()
+            else:
+                print("Starting game")
+                conn.send(b'Let\'s play hangman')
+                while True:
+                    data = conn.recv(1024)
+                    if not data: break
+                    respond_to_user(data, conn)
+
+
+if __name__ == "__main__":
+    try:
+        play_hangman()
+    except socket.error as err:
+        print("Unable to create the socket on " + str(HOST) + ":" + str(PORT) + "\n" + str(err))
