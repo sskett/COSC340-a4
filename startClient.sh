cf36a71ae0844acc30f3e7a9dc08902512a40bd4
Spencer Skett <sskett@gmail.com>
2020-04-27 16:38:19 +0800

    Commented the shell scripts

---
+++ startClient.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## COSC340 - A2
+## Spencer Skett
+## A networked word guessing game
+
+# Runs the game by attempting to connect to the specified host ($1) and port ($2)
+./client.py $1 $2
---
+++ startServer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## COSC340 - A2
+## Spencer Skett
+## A networked word guessing game
+
+# Starts the game server using the port specified by the first additional argument
+./server.py $1
