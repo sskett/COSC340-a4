53937b5040d1150ab8514e37b7d28f8b1b1574b0
Spencer Skett <sskett@gmail.com>
2020-04-27 16:31:06 +0800

    Complete?
    Incorporate separation of score and game over messages if possible?

--- client.py
+++ client.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
+# Import dependencies
 import sys
 import socket
-import unicodedata
 
-# Define globals
+# Set up required variables
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
+MSGLEN = 2048
 encoding = 'ascii'
 active = False
 
 
 def close_conn(s):
+    """Properly closes the socket connection.
+    Keyword arguments:
+    s -- a socket connection
+    """
     try:
         s.shutdown(socket.SHUT_RDWR)
     except socket.error as shutdown_err:
@@ -23,30 +28,37 @@ def close_conn(s):
 
 
 def start_game(s):
+    """Completes initial start up process by connecting to a game server.
+    Keyword arguments:
+    s -- a local socket
+    """
     try:
         s.connect((HOST, PORT))
         s.sendall("START GAME".encode(encoding))
-        data = ""
-        if data == "Invalid start request. Disconnecting...":
-            return False
-        else:
-            print(data)
-            return True
+        return True
     except socket.error as err:
         print(err)
         close_conn(s)
         return False
 
 
-def is_number(string):
+def check_gameover(string):
+    """Checks the server response to determine if the game has completed,
+    Keyword arguments:
+    string -- a decoded message received from the game server
+    """
     try:
-        int(string)
+        int(string[0])
         return True
     except ValueError:
         return False
 
 
 def take_user_input(s):
+    """Prompts the user for their letter/word guess or a user-requested exit.
+    Keyword arguments:
+    s -- a socket connection to a game server
+    """
     while True:
         entry = input("What's your guess?\n")
         if entry.isalpha():
@@ -60,36 +72,69 @@ def take_user_input(s):
     return entry
 
 
-def play_game(s):
-    data = ""
-    data = (s.recv(1024)).decode(encoding)
+def send_guess(s):
+    """Takes a char string input by the user, converts it into an ASCII encoded byte-form and sends to
+    the game server.
+    Keyword arguments:
+    s -- a socket connection to the game server
+    """
+    try:
+        entry = take_user_input(s)
+        s.sendall(entry.lower().encode(encoding))
+    except socket.error as err:
+        print("Connection lost:", err)
+        s.close()
+        return False
+    except KeyboardInterrupt as user_err:
+        print("\nGame process terminated by user.\n", user_err)
+        s.close()
+        return False
+    return True
+
+
+def receive_msg(s):
+    """Receives a message in byte form from the game server and decodes it into an ASCII based string.
+    Keyword arguments:
+    s -- a socket connection to the game server
+    """
+    try:
+        data = (s.recv(MSGLEN)).decode(encoding)
+        return data
+    except socket.error as err:
+        print("Server communication error.\n", err)
+        close_conn(s)
+        return False
+
+
+def process_guess(s):
+    """If there is still an active connection, receives and displays to the user a masked string
+    representing the current state (ie known/unknown letters) of the game word, then responds to the
+    server with the player's next guess.
+    Keyword arguments:
+    s -- a socket connection to a game server
+    """
+    data = receive_msg(s)
     if len(data) == 0:
         print("Sorry, connection to the server has been lost. Exiting game...")
         s.close()
         return False
-    elif is_number(data):
+    elif data[0].isupper():
+        print("Server response error. Disconnecting.")
+        close_conn(s)
+        return False
+    elif check_gameover(data):
         print("Your score is:", data)
-        #TODO: Need to process fixed length messages to split score from GAME OVER signal
-        data = (s.recv(1024)).decode(encoding)
-        print(data)
         close_conn(s)
         return False
     else:
         print(str(data))
-        try:
-            entry = take_user_input(s)
-            s.sendall(entry.lower().encode(encoding))
-        except socket.error as err:
-            print("Connection lost:", err)
-            s.close()
-            return False
-        return True
+        return send_guess(s)
 
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         active = start_game(s)
         while active:
-            active = play_game(s)
+            active = process_guess(s)
 
 
--- server.py
+++ server.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
 
+# Import dependencies
 import sys
 import socket
 import random
 
-
+# Set up required variables
 HOST = 'localhost'
 PORT = int(sys.argv[1])
 MAX_CONNS = 1
+MSGLEN = 2048
 encoding = 'ascii'
 
-result = []
 
-guesses = []
-word_guesses = []
-char_count = 0
-word_count = 0
+def create_socket():
+    """Creates and configures a socket for use by the game server."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(MAX_CONNS)
+    return s
 
 
 def create_word_list(filename):
+    """Selects a random word from a given text file.
+    The input file should have one word per line.
+
+    Keyword arguments:
+    filename -- a text file containing one word per line (a-z chars only)
+    """
     file = open(filename, "r")
     word_list = file.read().splitlines()
     file.close()
@@ -27,13 +37,22 @@ def create_word_list(filename):
 
 
 def close_conn(s):
+    """Properly closes the socket
+    Keyword arguments:
+    s -- a socket connection
+    """
     s.shutdown(socket.SHUT_RDWR)
     s.close()
 
 
-def letter_guess(guess,display):
-    global char_count
-    char_count += 1
+def letter_guess(guess, display):
+    """Checks if the guessed letter is in the word and uncovers any matching blank spaces for the player if so.
+    Keyword arguments:
+    guess -- the player's guess, which should be a single character in the range a-z
+    display -- the current 'blankified' representation of the game word that has been displayed to the player
+    """
+    global char_guesses_count
+    char_guesses_count += 1
     if guess in guesses:
         return
     else:
@@ -44,71 +63,124 @@ def letter_guess(guess,display):
 
 
 def word_guess(guess, display):
-    global word_count
-    word_count += 1
-    if guess in word_guesses:
-        return display
+    """Checks if the player's guess matches the word and triggers the end game if it does.
+    Keyword arguments:
+    guess -- the player's guess, which should be a single word string consisting of characters from a-z
+    display -- the current 'blankified' representation of the game word that has been displayed to the player
+    """
+    global word_guesses_count
+    word_guesses_count += 1
+    if guess == word:
+        return word
     else:
-        i = 0
-        for c in guess:
-            if c == word[i]:
-                i += 1
-            else:
-                break
-        if i == len(word):
-            return word
-        else:
-            return display
+        return display
 
 
 def send_msg(msg, conn):
-    # TODO: Need to incorporate fixed length msg to prevent "GAME OVER" being sent with score
+    """Appends a newline to the base message string and sends the message using ASCII encoding.
+    Keyword arguments:
+    msg -- a string that is to be sent to the connected user
+    conn -- a socket connection
+    """
     msg = msg + "\n"
     conn.sendall(msg.encode(encoding))
 
 
 def arr_to_str(arr):
+    """Converts an array to a string.
+    Keyword arguments:
+    arr -- an array of characters, eg representing the characters in the game word
+    """
     to_string = ""
     for c in arr:
         to_string += c
     return to_string
 
 
-def play_hangman(display):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((HOST, PORT))
-        s.listen(MAX_CONNS)
-        conn, addr = s.accept()
-        with conn:
-            data = conn.recv(1024)
-            if not data.decode(encoding) == 'START GAME':
+def calc_score():
+    """Calculates the player's score based on the difficulty of the word and how many guesses they needed."""
+    score = (len(word) * 10) - (char_guesses_count * 2) - word_guesses_count
+    return str(score)
+
+
+def setup_game(addr, display):
+    """Outputs connection details to the server terminal and prepares a blanked version of the word to display.
+    Keyword arguments:
+    addr -- the IP address of the user connected to the socket
+    display -- an array to store the blank character representation of the game word
+    """
+    print("Starting game. Connected with:", addr)
+    for c in word:
+        display.append("_")
+    print("The word is:", word)
+
+
+def run_game(conn, display):
+    """Outputs the current display word to the player with unknown letters shown as '_'.
+     Loops until the player guesses the word or has guessed every letter.
+    Keyword arguments:
+    conn -- a socket connection
+    display -- the array of masked characters representing the game word being displayed to the user
+    """
+    while "_" in display:
+        send_msg(arr_to_str(display), conn)
+        #try:
+        data = conn.recv(MSGLEN)
+        #except socket.error as err:
+         #   print("Connection from user lost.\n", err)
+          #  close_conn(conn)
+           # break
+        guess = data.decode(encoding)
+        if len(guess) > 1:
+            display = word_guess(guess, display)
+        else:
+            letter_guess(guess, display)
+
+
+def end_game(conn):
+    """Output appropriate messages to signal the end of the game.
+    Keyword arguments:
+    conn -- a socket connection
+    """
+    send_msg(calc_score(), conn)
+    send_msg("GAME OVER", conn)
+    print("Game ended.\n")
+
+
+def play_hangman(s):
+    """Creates a socket for a single-user and manages the game state.
+    Keyword arguments:
+    s -- a server socket which will connect to a player
+    """
+    conn, addr = s.accept()
+    with conn:
+        letters = []
+        data = conn.recv(MSGLEN)
+        if not data.decode(encoding) == 'START GAME':
+            close_conn(s)
+        else:
+            setup_game(addr, letters)
+            try:
+                run_game(conn, letters)
+                end_game(conn)
+            except socket.error as err:
+                print("Connection to user lost.\n", err)
                 close_conn(s)
-            else:
-                print("Starting game. Connected with:", addr)
-                for c in word:
-                    display.append("_")
-                print("The word is:", word)
-                while "_" in display:
-                    send_msg(arr_to_str(display), conn)
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    guess = data.decode(encoding)
-                    if len(guess) > 1:
-                        display = word_guess(guess, display)
-                    else:
-                        letter_guess(guess, display)
-                score = 10 * len(word) - 2 * char_count - word_count
-                send_msg(str(score), conn)
-                #send_msg("GAME OVER", conn)
-                print("Game ended.\n")
-        close_conn(s)
+                return
+    close_conn(s)
 
 
 if __name__ == "__main__":
-    word = create_word_list("wordList.txt")
-    try:
-        play_hangman(result)
-    except socket.error as err:
-        print("Unable to create the socket on " + str(HOST) + ":" + str(PORT) + "\n" + str(err))
+    while True:
+        s = create_socket()
+        word = create_word_list("wordList.txt")
+        guesses, word_guesses = [], []
+        char_guesses_count, word_guesses_count = 0, 0
+        try:
+            play_hangman(s)
+        except socket.error as err:
+            print("Unable to create the socket on " + str(HOST) + ":" + str(PORT) + "\n" + str(err))
+        except KeyboardInterrupt:
+            print("\nServer process terminated by user.")
+            close_conn(s)
+            break
